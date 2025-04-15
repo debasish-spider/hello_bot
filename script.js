a902d62d7845ff768d6cca5ac2d697b195df2384
Debasish Mondal <debasishme7@gmail.com>
2025-04-15 16:49:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const target = document.querySelector('.nxt-listing-title');
+
+if (target) {
+  const newDiv = document.createElement('div');
+  newDiv.className = 'tm-test';
+  newDiv.style.fontWeight = 'bold';
+  newDiv.textContent = 'hello world';
+  
+  target.appendChild(newDiv);
+}
